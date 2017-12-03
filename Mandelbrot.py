2c21c99937582dc8c5e0327904194ec3ea28e69c
Hans Weeks <hhweeks@Hanss-MacBook-Pro.local>
2017-12-03 13:39:01 -0700

    mp using python multiprocessing, issues joining arrays for NUM_PROCS>2

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,33 +1,11 @@
+def testConv(c, maxiter):#test complex c for diverges
+    n=0#count iterations
+    z=c
 
-class Mandelbrot:
-    def __init__(self):
-        self.maxiter=100
-        self.width=10
-        self.z0=0
-
-    def testConv(self, c):#test complex c for diverges
-        n=0
-        z=c
-        while(abs(z)<=2 and n<self.maxiter):
-            z=z*z+c
-            n+=1
-            #print("iter:{},abs{}.".format(n,abs(z)))
-        if(abs(z)>2):
-            return False
-        return True
-
-    def run(self,width):
-        self.width=width
-        halfwidth=width/2
-        print("1/2 width={}".format(halfwidth))
-
-        for i in range(width):
-            for j in range(width):
-                re=(i-halfwidth)/halfwidth
-                im=(j-halfwidth)/halfwidth
-                c = complex(re,im)
-                if self.testConv(c):
-                    print(c)
-
-man = Mandelbrot()
-man.run(100)
+    while(abs(z)<=2 and n< maxiter):
+        z=z*z+c
+        n+=1
+        #print("iter:{},abs{}.".format(n,abs(z)))
+    if(abs(z)>2):#catch diverging z on n=maxiter?
+        return False
+    return True
--- imdraw.py
+++ imdraw.py
@@ -1,13 +1,12 @@
 from PIL import Image, ImageDraw
-from Mandelbrot import Mandelbrot
+from Mandelbrot import testConv
 width=1000
 height=1000
 xstart=-2
 xend=1
 ystart=-1.5
 yend=1.5
-
-man=Mandelbrot()
+maxiter=255#max int we can represent as a pixel value
 
 image = Image.new('RGB', (width,height),(0,0,0))
 draw = ImageDraw.Draw(image)
@@ -19,7 +18,7 @@ for i in range(0,width):
         c = complex(re,im)
 
         color=255
-        if man.testConv(c):
+        if testConv(c, maxiter):
             color=0
         draw.point([i,j],(color,color,color))
 
